28dd4329a2c171b80fd501d36f12c8287ee9bbf8
Niccar95 <niccar.95@outlook.com>
2024-02-03 14:23:20 +0100

    ghghgh

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 // vite.config.ts
 import checker from 'vite-plugin-checker'
 export default {
-  base: "/Min-Portfolio/",
+  base: "/",
   plugins: [
     checker({
       // e.g. use TypeScript check
