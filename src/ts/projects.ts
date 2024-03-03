ba76e37996ae157ad72312d9082b7e96a5b77a2d
Niccar95 <niccar.95@outlook.com>
2024-03-03 17:39:05 +0100

    sdsds

--- src/ts/projects.ts
+++ src/ts/projects.ts
@@ -5,7 +5,12 @@ const allProjectsSection = document.getElementById("allProjectsSection");
 
 const todoList = new Project(1, "To-Do List", "blah", "todoList.png");
 
-const pokeSearch = new Project(2, "pokeSearch", "bleh", "pokeSearch.png");
+const pokeSearch = new Project(
+  2,
+  "pokeSearch",
+  "bleh",
+  "pokeSearch-985467a5.png"
+);
 
 let projectList = [todoList, pokeSearch];
 
