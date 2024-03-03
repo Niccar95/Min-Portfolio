25b68d7929722d4e056bdc0babd3b4abb530b095
Niccar95 <niccar.95@outlook.com>
2024-03-03 19:00:39 +0100

    jkj

--- src/ts/projects.ts
+++ src/ts/projects.ts
@@ -3,14 +3,9 @@ import { Project } from "./models";
 
 const allProjectsSection = document.getElementById("allProjectsSection");
 
-const todoList = new Project(1, "To-Do List", "blah", "/src/img/todoList.png");
+const todoList = new Project(1, "To-Do List", "blah", "/todoList.png");
 
-const pokeSearch = new Project(
-  2,
-  "pokeSearch",
-  "bleh",
-  "/src/img/pokeSearch.png"
-);
+const pokeSearch = new Project(2, "pokeSearch", "bleh", "/pokeSearch.png");
 
 let projectList = [todoList, pokeSearch];
 
@@ -22,6 +17,8 @@ projectList.forEach((project) => {
   const projectImage = document.createElement("img");
 
   projectImage.src = project.img;
+
+  console.log(todoList.img);
 
   projectContainer.className = "projectContainer";
 
