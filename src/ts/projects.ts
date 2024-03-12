2095febb2b09a7eca9a57826ebae63cbb0a8f925
Niccar95 <niccar.95@outlook.com>
2024-03-12 17:20:02 +0100

    fixed paths to images

--- src/ts/projects.ts
+++ src/ts/projects.ts
@@ -1,19 +1,14 @@
 import "./../scss/projects.scss";
 import { Project } from "./models";
 
-//import todoListImage from "/src/img/todoList.png";
-//import pokeSearchImage from "/src/img/pokeSearch.png";
+import todoListImage from "/src/img/todoList.png";
+import pokeSearchImage from "/src/img/pokeSearch.png";
 
 const allProjectsSection = document.getElementById("allProjectsSection");
 
-const todoList = new Project(1, "To-Do List", "blah", "../img/todoList.png");
+const todoList = new Project(1, "To-Do List", "blah", todoListImage);
 
-const pokeSearch = new Project(
-  2,
-  "pokeSearch",
-  "bleh",
-  "../img/pokeSearch.png"
-);
+const pokeSearch = new Project(2, "pokeSearch", "bleh", pokeSearchImage);
 
 let projectList = [todoList, pokeSearch];
 
